9c5dbf8747b1acaa944bee9feda5ee31534f0225
chentt0914 <2218861106@qq.com>
2022-08-11 22:04:04 +0800

    feat: 增加登录页面 接口联调

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
+import router from './router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//字体图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+//全局样式表
+import './assets/css/base.css'
 
-createApp(App).use(router).mount('#app')
+
+
+
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
