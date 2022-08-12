eac0ff6d2c8f6e6879b0e7ab281af3e218c9fb3a
chentt0914 <2218861106@qq.com>
2022-08-12 11:04:00 +0800

    增加了退出按钮

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,7 +3,7 @@
     <div class="login-box">
       <!-- 头部 -->
       <div class="header">
-        <img src="../assets/logo.png" alt="">
+        <img src="../assets/logo.png" alt="" />
       </div>
       <!-- 表单 -->
       <el-form class="login-form" label-width="0px" :model="form" :rules="rules" ref="resetForm">
@@ -18,116 +18,106 @@
             <el-button type="primary" @click="checkForm">登录</el-button>
             <el-button type="info" @click="resetForm">重置</el-button>
           </div>
-
         </el-form-item>
       </el-form>
-
     </div>
-    <i class="iconfont icon-showpassword" style="width: 50px;"></i>
+    <i class="iconfont icon-showpassword" style="width: 50px"></i>
   </div>
 </template>
 
 <script>
-import { Login } from "@/network/request"
-
-import { getCurrentInstance } from 'vue'
-import { ElMessage } from 'element-plus'
+import { Login } from '@/network/request';
+import { ElMessage } from 'element-plus';
 
 export default {
-  name:'Login',
+  name: 'Login',
   data() {
     return {
       //表单数据绑定对对象
       form: {
         username: 'admin',
-        password:'123456'
+        password: '123456',
       },
-       //表单的验证规则对象
+      //表单的验证规则对象
       rules: {
         //验证用户名
         username: [
           { required: true, message: '请输入用户名', trigger: 'blur' },
-          { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+          { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' },
         ],
         // 验证密码
         password: [
           { required: true, message: '请选择活动区域', trigger: 'change' },
-          { min: 6, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' }
-        ]
-      }
+          { min: 6, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' },
+        ],
+      },
     };
   },
-  methods:{
+  methods: {
     // 表单重置
-    resetForm(){
-      this.$refs.resetForm.resetFields()
+    resetForm() {
+      this.$refs.resetForm.resetFields();
     },
-    checkForm(){
-      this.$refs.resetForm.validate(async valid =>{
-      if(!valid) return
-       Login(this.form).then(res => {
-         console.log(res);
-         if (res.data.meta.status == 200) {
-           const userInfo = JSON.stringify(res.data.data)
-           localStorage.setItem("userInfo",userInfo);
-           this.$router.push('/')
-        }else{
-          ElMessage.error(res.data.meta.msg)
-        }
-
-       })
-      })
-    }
-  }
-}
+    checkForm() {
+      this.$refs.resetForm.validate(async valid => {
+        if (!valid) return;
+        Login(this.form).then(res => {
+          console.log(res);
+          if (res.data.meta.status == 200) {
+            ElMessage.success(res.data.meta.msg);
+            localStorage.setItem('token', res.data.data.token);
+            this.$router.push('/');
+          } else {
+            ElMessage.error(res.data.meta.msg);
+          }
+        });
+      });
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
-
-
-.login{
+.login {
   height: 100%;
   background-color: #2b4b6b;
-  .login-box{
+  .login-box {
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%,-50%);
+    transform: translate(-50%, -50%);
     width: 450px;
     height: 300px;
     background-color: #fff;
     .header {
-        position: absolute;
-        left: 50%;
-        transform: translate(-50%,-50%);
-        width: 130px;
-        height: 130px;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 130px;
+      height: 130px;
+      border-radius: 50%;
+      padding: 10px;
+      border: 1px solid #eee;
+      background-color: #fff;
+      box-shadow: 0 0 10px #ddd;
+      img {
+        width: 100%;
         border-radius: 50%;
-        padding: 10px;
-        border: 1px solid #eee;
-        background-color: #fff;
-        box-shadow: 0 0 10px #ddd;
-        img{
-          width: 100%;
-          border-radius: 50%;
-          background-color: #eee;
-        }
+        background-color: #eee;
       }
-    .login-form{
+    }
+    .login-form {
       width: 100%;
       position: absolute;
       bottom: 0;
       padding: 0 20px;
       box-sizing: border-box;
-      .btn{
+      .btn {
         width: 100%;
         display: flex;
         justify-content: flex-end;
-
       }
     }
   }
-
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -17,16 +17,20 @@ const router = createRouter({
   routes
 })
 
+// 挂载路由导航守卫
+// to将要访问的路径
+// from从哪里跳转来
+// next是一个函数 next（）放行    next（'/'）强制跳转
 router.beforeEach((to, from, next) => {
-  const userInfo = localStorage.getItem('userInfo')
+  const token = localStorage.getItem('token')
   if (to.path == '/login'){
-    if(!userInfo) next()
+    if(!token) next()
     else {
       next({path:'/'})
     }
   }
   else {
-    if(userInfo){
+    if(token){
       next()
     }else{
       next({path:'/login'})
